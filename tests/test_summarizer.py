fed5a9d60a2c145460c92178a7b04972f0054945
crodriguez1a <crodriguez1a@gmail.com>
2020-07-01 17:23:52 -0400

    [0.1.4] Adds single sentence summarization

--- tests/test_summarizer.py
+++ tests/test_summarizer.py
@@ -13,3 +13,10 @@ def test_single_sentence():
     result = bpe_summarize(blob)
 
     assert result == "notification repeated overdraft"
+
+
+def test_sentence_defaults_to_mean():
+    blob = "I received a notification today about being subject to extended holds due to repeated overdrafts."
+    result = bpe_summarize(blob, percentile=99)
+
+    assert result == "notification repeated overdraft"
