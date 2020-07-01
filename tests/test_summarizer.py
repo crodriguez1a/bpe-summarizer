d0f473be66c1777d3eec3f87cfdc7350708f6f65
crodriguez1a <crodriguez1a@gmail.com>
2020-07-01 17:33:48 -0400

    [0.1.5] Ensure percentile is deterministic

--- tests/test_summarizer.py
+++ tests/test_summarizer.py
@@ -20,3 +20,10 @@ def test_sentence_defaults_to_mean():
     result = bpe_summarize(blob, percentile=99)
 
     assert result == "notification repeated overdraft"
+
+
+def test_sentence_defaults_to_mean_only_when_percentile_less_than_mean():
+    blob = "I received a notification today about being subject to extended holds due to repeated overdrafts."
+    result = bpe_summarize(blob, percentile=50)
+
+    assert result == "notification subject extended holds repeated overdraft"
