0be2c6adcc9a8deb77bf198430ec06ee0fafe258
Carlos Rodriguez <crodriguez1a@gmail.com>
2020-07-01 17:45:08 -0400

    Adds single sentence summarization (#3)
    * Adds single sentence summarization
    
    * Expand Python version compatibility
    
    * Formatting
    
    * Address flake
    
    * [0.1.5] Adds single sentence summarization
    
    * formatting

--- tests/test_summarizer.py
+++ tests/test_summarizer.py
@@ -1,8 +1,29 @@
 from src.bpe_summarizer import bpe_summarize
 
 
-def test_small_text():
+def test_fewer_sentences():
     blob = "The most meaningful part of this sentence is here. I frost apples. Bannas whwere foo."
     result = bpe_summarize(blob)
 
     assert result == "The most meaningful part of this sentence is here."
+
+
+def test_single_sentence():
+    blob = "I received a notification today about being subject to extended holds due to repeated overdrafts."
+    result = bpe_summarize(blob)
+
+    assert result == "notification repeated overdraft"
+
+
+def test_sentence_defaults_to_mean():
+    blob = "I received a notification today about being subject to extended holds due to repeated overdrafts."
+    result = bpe_summarize(blob, percentile=99)
+
+    assert result == "notification repeated overdraft"
+
+
+def test_sentence_defaults_to_mean_only_when_percentile_less_than_mean():
+    blob = "I received a notification today about being subject to extended holds due to repeated overdrafts."
+    result = bpe_summarize(blob, percentile=50)
+
+    assert result == "notification subject extended holds repeated overdraft"
